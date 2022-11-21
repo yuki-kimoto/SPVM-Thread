ce0df66686ba317b2405b2ce49f821c55f8b4485
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-11-21 10:15:07 +0900

    Implement new and join

--- lib/SPVM/Cpp/Thread.cpp
+++ lib/SPVM/Cpp/Thread.cpp
@@ -10,7 +10,7 @@ do_worker1 () {
 
 extern "C" {
 
-static const char* FILE_NAME = "Thread.cpp";
+static const char* FILE_NAME = "Cpp/Thread.cpp";
 
 int32_t SPVM__Cpp__Thread__test(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
@@ -20,6 +20,46 @@ int32_t SPVM__Cpp__Thread__test(SPVM_ENV* env, SPVM_VALUE* stack) {
   t1.join();
                 
   return 0;
+}
+
+static void handler (SPVM_ENV* env, SPVM_VALUE* stack, void* obj_handler) {
+  
+  env->call_instance_method_by_name(env, stack, obj_handler, "", 0, FILE_NAME, __LINE__);
+}
+
+int32_t SPVM__Cpp__Thread__new(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e;
+  
+  void* obj_handler = stack[0].oval;
+  
+  std::thread* nt_thread = (std::thread*)env->alloc_memory_block_zero(env, sizeof(std::thread));
+  
+  SPVM_VALUE* thread_stack = env->new_stack(env);
+  
+  *nt_thread = std::thread(handler, env, thread_stack, obj_handler);
+  
+  void* obj_thread = env->new_pointer_by_name(env, stack, "Cpp::Thread", nt_thread, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+
+  stack[0].oval = obj_thread;
+}
+
+int32_t SPVM__Cpp__Thread__join(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t e;
+  
+  void* obj_thread = stack[0].oval;
+  
+  std::thread* nt_thread = (std::thread*)env->get_pointer(env, stack, obj_thread);
+  
+  try {
+    nt_thread->join();
+  }
+  catch (std::exception& cpp_exception){
+    env->die(env, stack, "[System Error]join failed:%s", cpp_exception.what(), FILE_NAME, __LINE__);
+    return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
+  }
 }
 
 }
---
+++ lib/SPVM/Cpp/Thread/Handler.pm
@@ -0,0 +1,43 @@
+package SPVM::Cpp::Thread::Handler;
+
+1;
+
+=head1 Name
+
+SPVM::Cpp::Thread::Handler - Cpp::Thread::Handler is a SPVM module
+
+=head1 Usage
+
+  use Cpp::Thread::Handler;
+  
+=head1 Description
+
+C<Cpp::Thread::Handler> is a L<SPVM> module.
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
