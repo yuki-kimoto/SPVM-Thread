42a4d1ae7e229c6f046f8f79ab5444efcb3fb0b3
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-11-21 10:33:24 +0900

    Fixed thread

--- lib/SPVM/Cpp/Thread.cpp
+++ lib/SPVM/Cpp/Thread.cpp
@@ -12,19 +12,11 @@ extern "C" {
 
 static const char* FILE_NAME = "Cpp/Thread.cpp";
 
-int32_t SPVM__Cpp__Thread__test(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-  
-  std::thread t1(do_worker1);
-  t1.join();
-                
-  return 0;
-}
-
 static void handler (SPVM_ENV* env, SPVM_VALUE* stack, void* obj_handler) {
   
   env->call_instance_method_by_name(env, stack, obj_handler, "", 0, FILE_NAME, __LINE__);
+  
+  return;
 }
 
 int32_t SPVM__Cpp__Thread__new(SPVM_ENV* env, SPVM_VALUE* stack) {
@@ -38,7 +30,7 @@ int32_t SPVM__Cpp__Thread__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   SPVM_VALUE* thread_stack = env->new_stack(env);
   
   *nt_thread = std::thread(handler, env, thread_stack, obj_handler);
-  
+
   void* obj_thread = env->new_pointer_by_name(env, stack, "Cpp::Thread", nt_thread, &e, FILE_NAME, __LINE__);
   if (e) { return e; }
 
@@ -57,6 +49,7 @@ int32_t SPVM__Cpp__Thread__join(SPVM_ENV* env, SPVM_VALUE* stack) {
     nt_thread->join();
   }
   catch (std::exception& cpp_exception){
+    
     env->die(env, stack, "[System Error]join failed:%s", cpp_exception.what(), FILE_NAME, __LINE__);
     return SPVM_NATIVE_C_CLASS_ID_ERROR_SYSTEM;
   }
