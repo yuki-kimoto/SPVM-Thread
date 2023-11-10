0e61ba74b825b1d1b30042e9fb043ee41d7afe61
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-10 11:03:41 +0900

    Add the doc of Thread.

--- lib/SPVM/Thread.pm
+++ lib/SPVM/Thread.pm
@@ -6,31 +6,62 @@ our $VERSION = '0.001';
 
 =head1 Name
 
-SPVM::Thread - Thread is a SPVM module
+SPVM::Thread - Native Thread
 
 =head1 Usage
 
   use Thread;
   
+  my $results = [0];
+  my $thread = Thread->new([$results : int[]] method : void () {
+    $results->[0] = 5;
+  });
+  
+  $thread->join;
+
 =head1 Description
 
-The C<Thread> class has methods to create threads.
+The C<Thread> class has methods to create native threads.
 
-=head1 Fields
-
-
+This class is a binding of L<std::thread in C++|https://en.cppreference.com/w/cpp/thread/thread>.
 
 =head1 Class Methods
 
+C<static method new : L<Thread|SPVM::Thread> ($task : L<Callback|SPVM::Callback>);>
 
+Creates a new native thread and run the task $task on it.
+
+And creates a new L<Thread|SPVM::Thread> object and returns it.
 
 =head1 Instance Methods
 
+=head2 join
 
+C<method join : void ();>
+
+Waits for the thread to finish.
+
+=head2 get_id
+
+C<method get_id : L<Thread::ID|SPVM::Thread::ID> ();>
+
+Gets the thread ID.
 
 =head1 Repository
 
-L<>
+L<SPVM::Thread - Github|https://github.com/yuki-kimoto/SPVM-Thread>
+
+=head2 See Also
+
+=over 2
+
+=item * L<Thread::ThisThread|SPVM::Thread::ThisThread>
+
+=item * L<Thread::ID|SPVM::Thread::ID>
+
+=item * L<std::thread in C++|https://en.cppreference.com/w/cpp/thread/thread>
+
+=back
 
 =head1 Author
 
