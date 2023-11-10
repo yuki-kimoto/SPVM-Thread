94f96216641a9578a891a16dc0168ab19f407c19
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-10 10:30:32 +0900

    Add the doc of Thread::ID.

--- lib/SPVM/Thread.pm
+++ lib/SPVM/Thread.pm
@@ -38,7 +38,7 @@ Yuki Kimoto C<kimoto.yuki@gmail.com>
 
 =head1 Copyright & License
 
-Copyright 2022-2022 [--user-name], all rights reserved.
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- lib/SPVM/Thread/ThisThread.pm
+++ lib/SPVM/Thread/ThisThread.pm
@@ -10,6 +10,8 @@ SPVM::Thread::ThisThread - Getting Current Thread Information
 
   use Thread::ThisThread;
   
+  my $current_thread_id = Thread::ThisThread->get_id;
+
 =head1 Description
 
 The C<Thread::ThisThread> class of L<SPVM> has methods to get information of the current thread.
@@ -22,7 +24,7 @@ Gets the thread ID of the current thread.
 
 =head1 Copyright & License
 
-Copyright 2022-2022 [--user-name], all rights reserved.
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
