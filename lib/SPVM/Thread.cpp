1386c3cb7accc7f888a903f4755a6e0ac453baa1
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-09-02 10:12:03 +0900

    Improve caller logic.

--- lib/SPVM/Thread.cpp
+++ lib/SPVM/Thread.cpp
@@ -12,29 +12,14 @@ extern "C" {
 
 static const char* FILE_NAME = "Thread.cpp";
 
-static void thread_handler (SPVM_ENV* env, SPVM_OBJ* obj_self, SPVM_OBJ* obj_task) {
+static void thread_handler(SPVM_ENV* env, SPVM_OBJ* obj_self, SPVM_OBJ* obj_task, const char* caller_func_name, const char* caller_file, int32_t caller_line) {
   
   int32_t error_id = 0;
   
   SPVM_VALUE* stack = env->new_stack(env);
   
-  SPVM_OBJ* obj_caller_info = env->get_field_object_by_name(env, stack, obj_self, "caller_info", &error_id, __func__, FILE_NAME, __LINE__);
-  assert(obj_caller_info);
-  assert(error_id == 0);
-  
-  SPVM_OBJ* obj_caller_method_abs_name = env->get_field_object_by_name(env, stack, obj_caller_info, "method_abs_name", &error_id, __func__, FILE_NAME, __LINE__);
-  assert(error_id == 0);
-  const char* caller_method_abs_name = env->get_chars(env, stack, obj_caller_method_abs_name);
-  
-  int32_t caller_line = env->get_field_int_by_name(env, stack, obj_caller_info, "line", &error_id, __func__, FILE_NAME, __LINE__);
-  assert(error_id == 0);
-  
-  SPVM_OBJ* obj_caller_file = env->get_field_object_by_name(env, stack, obj_caller_info, "file", &error_id, __func__, FILE_NAME, __LINE__);
-  assert(error_id == 0);
-  const char* caller_file = env->get_chars(env, stack, obj_caller_file);
-  
   stack[0].oval = obj_task;
-  env->call_instance_method_by_name(env, stack, "", 1, &error_id, caller_method_abs_name, caller_file, caller_line);
+  env->call_instance_method_by_name(env, stack, "", 1, &error_id, caller_func_name, caller_file, caller_line);
   
   if (error_id) {
     // Reconstruct the full exception message including stack trace.
@@ -69,7 +54,22 @@ int32_t SPVM__Thread__create(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   std::thread* nt_thread = (std::thread*)env->new_memory_block(env, stack, sizeof(std::thread));
   
-  *nt_thread = std::thread(thread_handler, env, obj_self, obj_task);
+  SPVM_OBJ* obj_caller_info = env->get_field_object_by_name(env, stack, obj_self, "caller_info", &error_id, __func__, FILE_NAME, __LINE__);
+  assert(obj_caller_info);
+  assert(error_id == 0);
+  
+  SPVM_OBJ* obj_caller_method_abs_name = env->get_field_object_by_name(env, stack, obj_caller_info, "method_abs_name", &error_id, __func__, FILE_NAME, __LINE__);
+  assert(error_id == 0);
+  const char* caller_method_abs_name = env->get_chars(env, stack, obj_caller_method_abs_name);
+  
+  SPVM_OBJ* obj_caller_file = env->get_field_object_by_name(env, stack, obj_caller_info, "file", &error_id, __func__, FILE_NAME, __LINE__);
+  assert(error_id == 0);
+  const char* caller_file = env->get_chars(env, stack, obj_caller_file);
+  
+  int32_t caller_line = env->get_field_int_by_name(env, stack, obj_caller_info, "line", &error_id, __func__, FILE_NAME, __LINE__);
+  assert(error_id == 0);
+  
+  *nt_thread = std::thread(thread_handler, env, obj_self, obj_task, caller_method_abs_name, caller_file, caller_line);
   
   env->set_pointer(env, stack, obj_self, nt_thread);
   
